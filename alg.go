84be164de1a8914474352abe7149355dfaefede3
Justin Bell <jbell@circle.com>
2019-04-10 17:09:16 -0400

    Add rsa alg

---
+++ alg.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+
+	"github.com/google/uuid"
+
+	"github.com/belljustin/hancock/models"
+)
+
+type Alg interface {
+	NewKey(owner string) *models.Key
+}
+
+type Rsa struct{}
+
+func (a *Rsa) NewKey(owner string) (*models.Key, error) {
+	key, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return nil, err
+	}
+
+	priv := x509.MarshalPKCS1PrivateKey(key)
+	pub := x509.MarshalPKCS1PublicKey(&key.PublicKey)
+
+	return &models.Key{
+		Id:        uuid.New(),
+		Algorithm: "rsa",
+		Owner:     owner,
+		Pub:       pub,
+		Priv:      priv,
+	}, nil
+}
